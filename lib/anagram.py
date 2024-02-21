bd0aae5d7970fbd89f180c62bf865fe1266524ca
Daniel Long <danielwlong618@gmail.com>
2024-02-20 22:25:57 -0600

    lab done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        anagrams = []
+        ana_word = sorted(self.word)
+
+        for word in list:
+            if len(word) == len(self.word) and word.lower() != self.word:
+                list_letters = sorted(word.lower())
+                if ana_word == list_letters:
+                    anagrams.append(word)
+
+        return anagrams
